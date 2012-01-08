9a427b7fc09a0c3e30506ece0fc2fcf551e39813
Freddie <freddieboi@gmail.com>
2012-01-08 20:26:46 +0100

    Setup assets

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Awesomeness
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Stockholm'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -55,5 +55,8 @@ module Awesomeness
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Faster asset precompiles, partially load the application
+    config.assets.initialize_on_precompile = false
   end
 end
